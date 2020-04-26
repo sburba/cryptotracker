e8275fda03e553920d8b0b87a72a442a7c374e36
Sam Burba <github@samburba.com>
2020-04-26 16:14:29 -0700

    Set up logging in the update_trade_volumes script

--- app/update_trade_volumes.py
+++ app/update_trade_volumes.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import asyncio
+import logging
+import sys
 
 import httpx
 
@@ -8,6 +10,7 @@ from app.di import make_deps
 
 async def main():
     async with httpx.AsyncClient() as client:
+        logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
         deps = make_deps(client)
         await deps.database.connect()
         await deps.currency_trade_service.update_trade_volumes()
